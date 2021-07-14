c100f38a83375893f9d6ffed9d76a9635f75980a
夏达 <xd90333@163.com>
2021-07-14 19:20:39 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
-
+    var btn = document.createElement('button');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
 
-    console.log(Data);
+    element.appendChild(btn);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 const path = require('path');
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //版本5.3.2 有问题 require(...).pathToFileURL is not a function
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //坑： 使用ES6解构语法
+
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+
+    plugins: [
+        // 坑 不是数组 是对象参数
+        new CleanWebpackPlugin({
+            path: './dist'
+        }),
+
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+    ],
+
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
+
+
 };
