ccb8e99a1767f66398a5b2785591e1873656857b
夏达 <xd90333@163.com>
2021-07-15 18:58:16 +0800

    开发

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,18 @@ module.exports = {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    // watch:true,
 
+    // 浏览器自动加载页面
+    devServer: {
+        contentBase: './dist' // 坑 webpack-cli的新版本对webpack-dev-server版本的不兼容 npm i webpack-cli@3.3.12 -D
+    },
     plugins: [
         // 坑 不是数组 是对象参数
         new CleanWebpackPlugin({
-            path: './dist'
+            path: './dist',
+            cleanStaleWebpackAssets: false, // "watch": "webpack --watch", 要加该参数，否则会删除dist里的html文件
         }),
 
         new HtmlWebpackPlugin({
@@ -24,7 +31,8 @@ module.exports = {
 
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
 
 
