8c7128c934c249e18221d50dee67af82cd52e756
夏达 <xd90333@163.com>
2021-07-16 14:11:31 +0800

    HMR-热替换

--- src/print.js
+++ src/print.js
@@ -1,5 +1,7 @@
 export default function printMe() {
-    console.log('I get called from print.js!');
-    // consolee.log('错误信息');
+    console.log('I get called from print.js 哈哈哈!');
+    console.error('错误信息');
+    // console.log('Updating print.js...哈哈11');
 
 }
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //版本5.3.2 有问题 require(...).pathToFileURL is not a function
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //坑： 使用ES6解构语法
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
     },
     devtool: 'inline-source-map',
     // watch:true,
 
     // 浏览器自动加载页面
     devServer: {
-        contentBase: './dist' // 坑 webpack-cli的新版本对webpack-dev-server版本的不兼容 npm i webpack-cli@3.3.12 -D
+        contentBase: './dist',// 坑 webpack-cli的新版本对webpack-dev-server版本的不兼容 npm i webpack-cli@3.3.12 -D
+        hot: true,  //模块热替换(Hot Module Replacement 或 HMR),它允许在运行时更新各种模块，而无需进行完全刷新
     },
     plugins: [
         // 坑 不是数组 是对象参数
@@ -25,9 +26,20 @@ module.exports = {
         }),
 
         new HtmlWebpackPlugin({
-            title: '管理输出'
-        })
+            title: '热替换'
+        }),
+        // new webpack.NamedModulesPlugin(), //5.0已经内置
+        new webpack.HotModuleReplacementPlugin()
+
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
 
     output: {
         filename: '[name].bundle.js',
